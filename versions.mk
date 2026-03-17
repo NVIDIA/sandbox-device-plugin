a694cc9a9d436294b8373008422e4785fa7aad7a
Rajat Chopra <rajatc@nvidia.com>
2026-03-17 13:38:16 -0700

    bump distroless base image version
    Signed-off-by: Rajat Chopra <rajatc@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 VERSION ?= v0.9.0
-DISTROLESS_BASE_IMAGE ?= nvcr.io/nvidia/distroless/go:v4.0.2-dev
+DISTROLESS_BASE_IMAGE ?= nvcr.io/nvidia/distroless/go:v4.0.3-dev
 BUILDER_IMAGE ?= nvcr.io/nvidia/cuda:13.2.0-base-ubi9
 GFD_IMAGE ?= nvcr.io/nvidia/k8s-device-plugin:v0.19.0
 
